5c5da8dda951ec0430ca089bd2ebe7aef2d5acbf
Conor Hopkins <conor.hopkins@ucdconnect.ie>
2018-03-01 21:39:55 +0000

    First commit

--- SolveLed/__init__.py
+++ SolveLed/__init__.py
@@ -1,7 +1,6 @@
 """SolveLed - led assignment for comp30670"""
+from numpy import size
 
 __version__ = '0.1.0'
 __author__ = 'Conor Hopkins <conor.hopkins@ucdconnect.ie>'
 __all__ = []
-
-here is some test text
--- SolveLed/main.py
+++ SolveLed/main.py
@@ -9,10 +9,16 @@ def main():
     parser.add_argument('--input')
     args = parser.parse_args()
 
-    text  = getFile(args.input)
-    length = int(text[0])
-    myArray = [[False]*length for i in range(length)]
-    print(myArray)
+    global text, ledGrid #, count
+    count = 0
+    text = getFile(args.input)
+    ledGrid = [[False]*int(text[0]) for i in range(int(text[0]))]
+
+    for line in text:
+       count += applyCommand(line)
+       print("Total lights on", count)
+    print(count)
+
 
 def getFile(input):
     tempFile = "{}/../temp/input.txt".format(os.path.dirname(os.path.realpath(__file__)))
@@ -24,3 +30,45 @@ def getFile(input):
     file = open(tempFile, 'r')
     textArray = [i for i in file.readlines()]
     return textArray
+
+def applyCommand(line):
+    pat = re.compile(r'.*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*')
+    command = re.match(pat, line)
+    if not command:
+        return 0
+
+    outer1, outer2 = int(command.group(2)), int(command.group(4))+1
+    inner1, inner2 = int(command.group(3)), int(command.group(5))+1
+    tempCount = 0
+
+    for i in [outer1, outer2, inner1, inner2]:
+        if i<0:
+            i=0
+        if i>999:
+            i=999
+
+    if command.group(1)=="turn on":
+       for i in range(outer1, outer2):
+           for j in range(inner1, inner2):
+               if ledGrid[i][j] == False:
+                   tempCount += 1
+               ledGrid[i][j] = True
+
+    elif command.group(1)=="turn off":
+       for i in range(outer1, outer2):
+           for j in range(inner1, inner2):
+               if ledGrid[i][j] == True:
+                   tempCount -= 1
+               ledGrid[i][j] = False
+
+    else:
+       for i in range(outer1, outer2):
+           for j in range(inner1, inner2):
+               if ledGrid[i][j] == False:
+                   tempCount += 1
+               else:
+                   tempCount -= 1
+               ledGrid[i][j] = not ledGrid[i][j]
+
+    print("number of lightts changed", tempCount)
+    return tempCount
