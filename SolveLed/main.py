befd801c1cc6d44f505745c4eb24ee14b1703fb7
Conor Hopkins <conor.hopkins@ucdconnect.ie>
2018-03-03 11:13:23 +0000

    Implemented applyCommand function, added some extra tests

--- SolveLed/main.py
+++ SolveLed/main.py
@@ -7,56 +7,51 @@ from shutil import copy
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input')
-#    parser.add_argument('--verbose')
     args = parser.parse_args()
 
-    global text, ledGrid, gridLength #, count
     text = getFile(args.input)
+    if text==0:
+        exit()
     gridLength = int(text[0])
     ledGrid = [[False]*gridLength for i in range(gridLength)]
     count = 0
 
     for line in text:
-       count += applyCommand(line)
-#       if args.verbose=="t":
+       count += applyCommand(line, ledGrid, gridLength)
     print("Final number of lights on:", count)
 
 
 def getFile(input):
     tempFile = "{}/../temp/input.txt".format(os.path.dirname(os.path.realpath(__file__)))
-    print(os.path.realpath(__file__))
     isURL = re.match(r'http://*', input)
+
     if isURL:
-        urlretrieve(input, tempFile)
+        try:
+            urlretrieve(input, tempFile)
+        except FileNotFoundError:
+            print("File not found, please check the URL and connection to the internet")
+            return 0
     else:
-        copy(input, tempFile)
+        try:
+            copy(input, tempFile)
+        except FileNotFoundError:
+            print("File not found, please check input file name and path")
+        return 0
+
     file = open(tempFile, 'r')
     textArray = [i for i in file.readlines()]
+    file.close()
     return textArray
 
-def applyCommand(line):
+def applyCommand(line, ledGrid, gridLength):
     pat = re.compile(r'.*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*')
     command = re.match(pat, line)
     if not command:
         return 0
 
-    outer1, outer2 = int(command.group(2)), int(command.group(4))+1
-    inner1, inner2 = int(command.group(3)), int(command.group(5))+1
     tempCount = 0
-
-    l = [outer1, outer2, inner1, inner2]
-#    commandList = []
-#    for i in l:
-#        if i<0:
-#            commandList.append(0)
-#            print("caught", i)
-#        elif i>gridLength:
-#            commandList.append(gridLength)
-#            print("caught", i)
-#        else:
-#            commandList.append(i)
-#    commandList = [i for i in l]
-    commandList = [0 if i<0 else gridLength if i>gridLength else i for i in l]
+    uncleanCommandList = [int(command.group(2)), int(command.group(4))+1, int(command.group(3)), int(command.group(5))+1]
+    commandList = [0 if i<0 else gridLength if i>gridLength else i for i in uncleanCommandList]
 
     if command.group(1)=="turn on":
        for i in range(commandList[0], commandList[1]):
--- tests/test_SolveLed.py
+++ tests/test_SolveLed.py
@@ -8,6 +8,7 @@ from SolveLed.main import *
 
 class TestSolveLed(unittest.TestCase):
 
+
     def test_GetLocalFile(self):
        text = getFile("test.txt")
        self.assertTrue(text != None)
@@ -17,38 +18,43 @@ class TestSolveLed(unittest.TestCase):
        text = getFile("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt")
        self.assertTrue(text != None)
 
-#    def test_GetPhonyFile(self):
-#        text = getFile("a non-existent file")
+    def test_getBadFile(self):
+        text = getFile("aMadeUpFile.txt")
+#        self.assertRaises(FileNotFoundError, getFile, "made-upFile.txt")
 
     def test_applyTurnOnCommand(self):
-        global ledGrid
+        gridLength = 5
         ledGrid =  [[False]*5 for i in range(5)]
-        count = applyCommand("turn on 0,2 through 3,4")
+        count = applyCommand("turn on 0,2 through 3,4", ledGrid, gridLength)
         self.assertTrue(count == 12)
 
     def test_applyTurnOffCommand(self):
-        ledGrid = [[True]*5 for i in range(5):
+        ledGrid = [[True]*5 for i in range(5)]
+        gridLength = 5
         count = 25
-        count += applyCommand("turn off 1,2 through 3,4")
+        count += applyCommand("turn off 1,2 through 3,4", ledGrid, gridLength)
         self.assertTrue(count == 25-9)
 
     def test_applySwitchCommand(self):
-        ledGrid = [[True, False]*2 for i in range(4):
+        ledGrid = [[True, False]*2 for i in range(4)]
+        gridLength = 4
         count = 8
-        count += applyCommand("switch 0,1 through 1,1")
+        count += applyCommand("switch 0,4 through 3,4", ledGrid, gridLength)
         self.assertTrue(count == 8)
 
+    def test_applyOutOfRangeCommand(self):
+        ledGrid = [[False]*2 for i in range(2)]
+        gridLength = 2
+        count = applyCommand("turn on -1,-2 through 3,3", ledGrid, gridLength)
+        self.assertTrue(count==4)
+
     def test_applyBadCommand(self):
-        ledGrid = [[True]*5 for i in range(5):
+        gridLength = 5
+        ledGrid = [[True]*5 for i in range(5)]
         count = 25
-        count += applyCommand("this is a bad command")
+        count += applyCommand("this is a bad command", ledGrid, gridLength)
         self.assertTrue(count==25)
 
-    def test_applyOutOfRangeCommand(self)
-        ledGrid = [[True]*5 for i in range(5):
-        count = 25
-        count += applyCommand("turn off -5,50, -10,20")
-        self.assertTrue(count==0)
 
 if __name__=="__main__":
     unittest.main()
